6cdeedd756d4224c2b1454e0f0e94c595ee8725a
Medivh <ext.zhu.yang@badanamu.com.cn>
2020-08-14 12:08:47 +0800

    refactor: optimize reponsewriter

--- responsewriter.go
+++ responsewriter.go
@@ -24,28 +24,38 @@ func (w *ResponseWriter) SetStatusCode(statusCode int) {
 	w.Raw.WriteHeader(statusCode)
 }
 
-func (w *ResponseWriter) Text(statusCode int, text string) error {
+func (w *ResponseWriter) Text(statusCode int, text string) {
 	w.Raw.Header().Set("Content-Type", "text/plain")
 	w.Raw.WriteHeader(statusCode)
-	_, err := io.WriteString(w.Raw, text)
-	return err
+	if _, err := io.WriteString(w.Raw, text); err != nil {
+		http.Error(w.Raw, err.Error(), http.StatusInternalServerError)
+	}
 }
 
-func (w *ResponseWriter) HTML(statusCode int, content string) error {
+func (w *ResponseWriter) HTML(statusCode int, content string) {
 	w.Raw.Header().Set("Content-Type", "text/html")
 	w.Raw.WriteHeader(statusCode)
-	_, err := io.WriteString(w.Raw, content)
-	return err
+	if _, err := io.WriteString(w.Raw, content); err != nil {
+		http.Error(w.Raw, err.Error(), http.StatusInternalServerError)
+	}
 }
 
-func (w *ResponseWriter) JSON(statusCode int, value interface{}) error {
+func (w *ResponseWriter) JSON(statusCode int, value interface{}) {
 	w.Raw.Header().Set("Content-Type", "application/json")
 	w.Raw.WriteHeader(statusCode)
-	return json.NewEncoder(w.Raw).Encode(value)
+	if  err := json.NewEncoder(w.Raw).Encode(value); err != nil {
+		http.Error(w.Raw, err.Error(), http.StatusInternalServerError)
+	}
 }
 
-func (w *ResponseWriter) XML(statusCode int, value interface{}) error {
+func (w *ResponseWriter) XML(statusCode int, value interface{}) {
 	w.Raw.Header().Set("Content-Type", "application/xml")
 	w.Raw.WriteHeader(statusCode)
-	return xml.NewEncoder(w.Raw).Encode(value)
+	if err := xml.NewEncoder(w.Raw).Encode(value); err != nil {
+		http.Error(w.Raw, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (w *ResponseWriter) Error(statusCode int, error string) {
+	http.Error(w.Raw, text, statusCode)
 }
