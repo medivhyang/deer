d6ef80fc6594cdcd67c2ee7d5e364bf74b3cdf78
Medivh <ext.zhu.yang@badanamu.com.cn>
2020-09-28 11:47:46 +0800

    feat: add handler next

--- handler.go
+++ handler.go
@@ -4,8 +4,12 @@ import "net/http"
 
 type HandlerFunc func(w *ResponseWriter, r *Request)
 
-func (handler HandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	handler(WrapResponseWriter(w), WrapRequest(r))
+func (h HandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h(WrapResponseWriter(w), WrapRequest(r))
+}
+
+func (h HandlerFunc) Next(w *ResponseWriter, r *Request) {
+	h.ServeHTTP(w.Raw, r.Raw)
 }
 
 func WrapHandlerFunc(h http.HandlerFunc) HandlerFunc {
